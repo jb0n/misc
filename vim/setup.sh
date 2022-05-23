8b40ae3d575ae1fcf9b91c3226d036db23257040
jb0n <bubble.mack@gmail.com>
2022-05-22 23:27:32 -0700

    starting fonts

--- vim/setup.sh
+++ vim/setup.sh
@@ -30,3 +30,13 @@ vim_plugin $VIM_GO_URL
 vim_plugin $ALE_URL
 vim_plugin $GIT_URL
 
+
+
+#TODO: .vimrc, ale config
+mkdir -p ~./fonts
+cd ~./fonts/
+wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+unzip Fira_Code_v6.2.zip
+fc-cache -f -v
+``
+
