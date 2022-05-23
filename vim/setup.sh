fff72acd40c6612eaaf59e66898f11a74ef6de4a
jb0n <bubble.mack@gmail.com>
2022-05-22 23:01:26 -0700

    hha hardcoded url. oops

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,21 +4,29 @@ set -e
 
 sudo apt-get install git vim
 VIM_PLUGINS_DIR=~/.vim/pack/plugins/start
+
+VIM_GO_URL=https://github.com/fatih/vim-go.git
 VIM_GO_DIR=$VIM_PLUGINS_DIR/vim-go
+
+ALE_URL=https://github.com/dense-analysis/ale.git
 ALE_DIR=$VIM_PLUGINS_DIR/ale
 
+GIT_URL=https://github.com/tpope/vim-fugitive.git
+GIT_DIR=$VIM_PLUGINS_DIR/fugitive
 
 function vim_plugin {
-    if [ ! -d $1 ]
+    if [ ! -d $2 ]
         then
-        echo "installing $1"
-        git clone --depth=1 https://github.com/fatih/vim-go.git $1
+        echo "installing $2"
+        git clone --depth=1 $1 $2
     else
-       echo "updating $1"
-       cd $1 && git pull
+       echo "updating $2"
+       cd $2 && git pull
     fi
 }
 
-vim_plugin $VIM_GO_DIR
-vim_plugin $ALE_DIR
+mkdir -p $VIM_PLUGINS_DIR
+vim_plugin $VIM_GO_URL $VIM_GO_DIR
+vim_plugin $ALE_URL $ALE_DIR
+vim_plugin $GIT_URL $GIT_DIR
 
